c565fa6c378893bda159fe77abd889b36f95d3e3
Belarbia Dihia <106841947+dihiabelarbia@users.noreply.github.com>
2025-05-21 22:04:05 +0200

    Update firebase.js

--- front-cv/src/firebase.js
+++ front-cv/src/firebase.js
@@ -5,12 +5,6 @@ import { getStorage } from "firebase/storage";
 
 // Ta configuration Firebase
 const firebaseConfig = {
-  apiKey: "AIzaSyBjfc2-f3K0WRFbIIb94j_fC4bSPPQZfe0",
-  authDomain: "cv-parser-de1d2.firebaseapp.com",
-  projectId: "cv-parser-de1d2",
-  storageBucket: "cv-parser-de1d2.appspot.com",
-  messagingSenderId: "1000134545680",
-  appId: "1:1000134545680:web:5e02e42d5e55b8e5f49595"
 };
 
 // Initialisation de Firebase
